cebc08178f821e48f95eecab0c6956e223c90879
tungagnaneswar <tungagnaneswar12@gmail.com>
2024-12-26 17:39:18 +0530

    pushing all

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import TaskForm from './components/TaskForm'
 
 export default function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Task Buddy</h1>
+      <TaskForm/>
     </div>
   )
 }
